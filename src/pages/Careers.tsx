50f878dd40f7874187418db875e6dd423508c1df
DARK OSINT <levianawulandari7@gmail.com>
2025-10-20 11:43:33 +0700

    Create Careers.tsx

---
+++ src/pages/Careers.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import JobList from '../components/JobList';
+
+const Careers: React.FC = () => {
+  return (
+    <div>
+      <h1>Careers at Sat Siber</h1>
+      <JobList />
+    </div>
+  );
+};
+
+export default Careers;
