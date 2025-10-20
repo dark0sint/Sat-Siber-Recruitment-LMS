56d7ae1e9013076674c4ac949c41c1f915921315
DARK OSINT <levianawulandari7@gmail.com>
2025-10-20 11:44:07 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Careers from './pages/Careers';
+
+function App() {
+  return (
+    <div className="App">
+      <Careers />
+    </div>
+  );
+}
+
+export default App;
